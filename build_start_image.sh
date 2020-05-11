73380890dc698a138fbc4aee4c4c61d91198258c
jerome arn <jerome.arn@heig-vd.ch>
2020-05-11 02:22:49 +0200

    tache 4

--- build_start_image.sh
+++ build_start_image.sh
@@ -2,7 +2,7 @@
 docker build -t res/apache_rp ./apache-reverse-proxy 
 docker build -t res/apache_static ./apache-php    
 docker build -t res/express ./express-images
-docker run -d res/apache_static
-docker run -d res/express
-docker run -d -p 8080:80 res/apache_rp 
+docker run -d --name static res/apache_static
+docker run -d --name express res/express
+docker run -d -p 8080:80 --name rp res/apache_rp 
 
