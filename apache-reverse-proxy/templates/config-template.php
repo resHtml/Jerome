c0d1cf0871389e257f80be9bf82e7e51bf12a10b
jerome arn <jerome.arn@heig-vd.ch>
2020-05-23 11:22:44 +0200

    config sticky

--- apache-reverse-proxy/templates/config-template.php
+++ apache-reverse-proxy/templates/config-template.php
@@ -8,9 +8,11 @@
 <VirtualHost *:80>
 	ServerName demo.res.ch
 	
+	Header add Set-Cookie "ROUTEID=.%{BALANCER_WORKER_ROUTE}e; path=/" env=BALANCER_ROUTE_CHANGED
 	<Proxy 'balancer://static'>
 		BalancerMember 'http://<?php print "$ipStatic1"?>' route=1
 		BalancerMember 'http://<?php print "$ipStatic2"?>' route=2
+		ProxySet stickysession=ROUTEID
 	</Proxy>
 	
 	<Proxy 'balancer://dynamic'>
@@ -18,14 +20,14 @@
 		BalancerMember 'http://<?php print "$ipDynamic2"?>' route=2
 	</Proxy>
 	
-<Location '/balancer-manager'>
-    ProxyPass !
-    
-    SetHandler balancer-manager
-    # Require host demo.res.ch
-    # Order Deny,Allow
-    # Allow from all
-  </Location>
+	<Location '/balancer-manager'>
+		ProxyPass !
+		
+		SetHandler balancer-manager
+		# Require host demo.res.ch
+		# Order Deny,Allow
+		# Allow from all
+  	</Location>
 
 	ProxyPass '/api/dynamic/' 'balancer://dynamic/api/dynamic/'
 	ProxyPassReverse '/api/dynamic/' 'balancer://dynamic/api/dynamic/'
