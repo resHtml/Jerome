d38ecc473a2ff2c1db03ad46820ba5d8571f92e1
jerome arn <jerome.arn@heig-vd.ch>
2020-06-02 22:58:44 +0200

    script presentation

--- build_start_image.sh
+++ build_start_image.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
-docker kill rp static express
+docker kill rp static1 static1
 docker rm `docker ps -qa` 
 docker build -t res/apache_rp ./apache-reverse-proxy 
 docker build -t res/apache_static ./apache-php    
 docker build -t res/express ./express-images
-docker run -d --name static res/apache_static
-docker run -d --name express res/express
-docker run -d -p 8080:80 --name rp res/apache_rp 
+
+docker run -d --name static1 res/apache_static
+export STATIC_APP1=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' static1)":80"
+echo "ip_static1: $STATIC_APP1"
+
+docker run -d --name static1 res/express
+export DYNAMIC_APP1=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' express1)":3000"
+echo "ip_dynamic1 :$DYNAMIC_APP1"
+
+
+docker run -d -e STATIC_APP1 -e DYNAMIC_APP1 -p 8080:80 --name rp res/apache_rp 
 
